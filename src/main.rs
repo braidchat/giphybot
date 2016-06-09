ee9001f2b3729a683e8a5e055a72dd3e54c51d0c
James N. V. Cash <james.nvc@gmail.com>
2016-06-09 10:45:11 -0400

    basic server running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+#[macro_use]
+extern crate iron;
+extern crate rmp_serialize as msgpack;
+extern crate rustc_serialize;
+
+use std::error::Error;
+use std::fmt;
+
+use iron::{Iron,Request,Response,Handler,IronResult,IronError};
+use iron::{method,status};
+use msgpack::{Decoder,Encoder};
+use rustc_serialize::{Decodable,Encodable};
+
+#[derive(Debug)]
+pub struct NoRoute;
+
+impl fmt::Display for NoRoute {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("No matching route found.")
+    }
+}
+
+impl Error for NoRoute {
+    fn description(&self) -> &str { "No Route" }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|request : &mut Request| {
+        let req_path = request.url.path.join("/");
+        match request.method {
+            method::Put => {
+                if req_path == "message" {
+                    let mac = request.headers.get_raw("X-Braid-Signature");
+                    println!("Request mac = {:?}", mac);
+                    Ok(Response::with((status::Ok, "ok")))
+                } else {
+                    Err(IronError::new(NoRoute, status::NotFound))
+                }
+            }
+            _ => Err(IronError::new(NoRoute, status::NotFound))
+        }
+    }).http("localhost:9999").unwrap();
 }
