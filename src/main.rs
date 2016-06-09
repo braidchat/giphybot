0c16bcb4e07b691f76bd903ff625948c14976cb0
James N. V. Cash <james.nvc@gmail.com>
2016-06-09 11:57:18 -0400

    put routing error into seperate mod

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate iron;
 extern crate rmp_serialize as msgpack;
 extern crate rustc_serialize;
 
-use std::error::Error;
-use std::fmt;
 use std::io::Read;
 
 use iron::{Iron,Request,Response,IronError};
@@ -12,19 +10,7 @@ use iron::{method,status,request};
 use msgpack::{Decoder};
 use rustc_serialize::{Decodable};
 
-// Route error handler
-#[derive(Debug)]
-pub struct NoRoute;
-
-impl fmt::Display for NoRoute {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("No matching route found.")
-    }
-}
-
-impl Error for NoRoute {
-    fn description(&self) -> &str { "No Route" }
-}
+mod routing;
 
 // Decoding transit+msgpack body
 fn decode_msgpack_body<'a, 'b>(req_body: &mut request::Body<'a, 'b>) {
@@ -48,10 +34,10 @@ fn main() {
                     decode_msgpack_body(&mut request.body);
                     Ok(Response::with((status::Ok, "ok")))
                 } else {
-                    Err(IronError::new(NoRoute, status::NotFound))
+                    Err(IronError::new(routing::NoRoute, status::NotFound))
                 }
             }
-            _ => Err(IronError::new(NoRoute, status::NotFound))
+            _ => Err(IronError::new(routing::NoRoute, status::NotFound))
         }
     }).http("localhost:9999").unwrap();
 }
---
+++ src/routing.rs
@@ -0,0 +1,16 @@
+use std::error::Error;
+use std::fmt;
+
+// Route error handler
+#[derive(Debug)]
+pub struct NoRoute;
+
+impl fmt::Display for NoRoute {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("No matching route found.")
+    }
+}
+
+impl Error for NoRoute {
+    fn description(&self) -> &str { "No Route" }
+}
