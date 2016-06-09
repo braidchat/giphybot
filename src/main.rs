5375b7c06b763554678b08b691dce30c30764a98
James N. V. Cash <james.nvc@gmail.com>
2016-06-09 11:28:41 -0400

    reading simple msgpack working

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ extern crate rustc_serialize;
 
 use std::error::Error;
 use std::fmt;
+use std::io::Read;
 
-use iron::{Iron,Request,Response,Handler,IronResult,IronError};
-use iron::{method,status};
-use msgpack::{Decoder,Encoder};
-use rustc_serialize::{Decodable,Encodable};
+use iron::{Iron,Request,Response,IronError};
+use iron::{method,status,request};
+use msgpack::{Decoder};
+use rustc_serialize::{Decodable};
 
+// Route error handler
 #[derive(Debug)]
 pub struct NoRoute;
 
@@ -24,6 +26,16 @@ impl Error for NoRoute {
     fn description(&self) -> &str { "No Route" }
 }
 
+// Decoding transit+msgpack body
+fn decode_msgpack_body<'a, 'b>(req_body: &mut request::Body<'a, 'b>) {
+    let mut buf = Vec::new();
+    req_body.read_to_end(&mut buf).unwrap();
+    let mut decoder = Decoder::new(&buf[..]);
+    let res: (u8, String) = Decodable::decode(&mut decoder).unwrap();
+    println!("result = {:?}", res);
+}
+
+// Main
 fn main() {
     Iron::new(|request : &mut Request| {
         let req_path = request.url.path.join("/");
@@ -31,7 +43,11 @@ fn main() {
             method::Put => {
                 if req_path == "message" {
                     let mac = request.headers.get_raw("X-Braid-Signature");
+                    // TODO: verify mac
                     println!("Request mac = {:?}", mac);
+                    {
+                        decode_msgpack_body(&mut request.body);
+                    }
                     Ok(Response::with((status::Ok, "ok")))
                 } else {
                     Err(IronError::new(NoRoute, status::NotFound))
