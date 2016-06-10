102a47185ad76a17397967bd8c195bbcc3142918
James N. V. Cash <james.nvc@gmail.com>
2016-06-09 21:03:33 -0400

    reading & writing with UUIDs working!

--- src/message.rs
+++ src/message.rs
@@ -5,19 +5,19 @@ use serde;
 use serde::{Serialize,Deserialize};
 use rmp_serde::{Serializer,Deserializer};
 
-#[derive(Debug, PartialEq, Deserialize)]
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
 struct Message {
-    #[serde(rename="~:id", deserialize_with="deserialize_transit_uuid")]
+    #[serde(rename="~:id", deserialize_with="deserialize_transit_uuid", serialize_with="serialize_transit_uuid")]
     id: Uuid,
-    #[serde(rename="~:group-id", deserialize_with="deserialize_transit_uuid")]
+    #[serde(rename="~:group-id", deserialize_with="deserialize_transit_uuid", serialize_with="serialize_transit_uuid")]
     group_id: Uuid,
-    #[serde(rename="~:thread-id", deserialize_with="deserialize_transit_uuid")]
+    #[serde(rename="~:thread-id", deserialize_with="deserialize_transit_uuid", serialize_with="serialize_transit_uuid")]
     thread_id: Uuid,
-    #[serde(rename="~:user-id", deserialize_with="deserialize_transit_uuid")]
+    #[serde(rename="~:user-id", deserialize_with="deserialize_transit_uuid", serialize_with="serialize_transit_uuid")]
     user_id: Uuid,
-    #[serde(rename="~:mentioned-user-ids", deserialize_with="deserialize_transit_uuid_seq")]
+    #[serde(rename="~:mentioned-user-ids", deserialize_with="deserialize_transit_uuid_seq", serialize_with="serialize_transit_uuid_seq")]
     mentioned_user_ids: Vec<Uuid>,
-    #[serde(rename="~:mentioned-tag-ids", deserialize_with="deserialize_transit_uuid_seq")]
+    #[serde(rename="~:mentioned-tag-ids", deserialize_with="deserialize_transit_uuid_seq", serialize_with="serialize_transit_uuid_seq")]
     mentioned_tag_ids: Vec<Uuid>,
     #[serde(rename="~:content")]
     content: String,
@@ -51,7 +51,25 @@ fn transit_to_uuid(transit_uuid: TransitUuid) -> Uuid {
     Uuid::from_bytes(&bytes).ok().unwrap()
 }
 
-fn uuid_to_transit(uuid: Uuid) -> TransitUuid {
+fn serialize_transit_uuid<S>(uuid: &Uuid, se: &mut S) -> Result<(), S::Error>
+where S: serde::Serializer {
+    let transit_uuid = uuid_to_transit(uuid);
+    match transit_uuid.serialize(se) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(serde::ser::Error::custom("Failed to serialize uuid")),
+    }
+}
+
+fn serialize_transit_uuid_seq<S>(uuids: &Vec<Uuid>, se: &mut S) -> Result<(), S::Error>
+where S: serde::Serializer {
+    let transit_uuids: Vec<TransitUuid> = uuids.into_iter().map(uuid_to_transit).collect();
+    match transit_uuids.serialize(se) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(serde::ser::Error::custom("Failed to serialize uuid vector")),
+    }
+}
+
+fn uuid_to_transit(uuid: &Uuid) -> TransitUuid {
     let bytes = uuid.as_bytes();
     let mut reader = Cursor::new(bytes);
     let hi64 = reader.read_i64::<BigEndian>().unwrap();
@@ -59,19 +77,46 @@ fn uuid_to_transit(uuid: Uuid) -> TransitUuid {
     ("~#u".to_string(), (hi64, lo64))
 }
 
-/*
 fn test_response() {
+    use std::io::prelude::*;
+    use std::fs::File;
+    //use std::io::Write;
+    use rmp::Marker;
+    use rmp::encode::{ValueWriteError, write_map_len, write_str};
+    use rmp_serde::encode::VariantWriter;
+
+    struct StructMapWriter;
+
+    impl VariantWriter for StructMapWriter {
+        fn write_struct_len<W>(&self, wr: &mut W, len: u32) -> Result<Marker, ValueWriteError>
+            where W: Write
+            {
+                write_map_len(wr, len)
+            }
+
+        fn write_field_name<W>(&self, wr: &mut W, _key: &str) -> Result<(), ValueWriteError>
+            where W: Write
+            {
+                write_str(wr, _key)
+            }
+    }
+
     let test_message = Message {
         id: Uuid::new_v4(),
         group_id: Uuid::new_v4(),
         thread_id: Uuid::new_v4(),
         user_id: Uuid::new_v4(),
-        mentioned_user_ids: vec![],
-        mentioned_tag_ids: vec![],
+        mentioned_user_ids: vec![Uuid::new_v4()],
+        mentioned_tag_ids: vec![Uuid::new_v4(), Uuid::new_v4()],
         content: "response back!".to_string()
     };
+
+    let mut buf = vec![];
+    test_message.serialize(&mut Serializer::with(&mut &mut buf, StructMapWriter)).ok().unwrap();
+    let mut f: File = File::create("message_from_bot.msgpack").ok().unwrap();
+    f.write_all(&buf[..]).ok().unwrap();
 }
-*/
+
 pub fn decode_msgpack_body(msgpack_buf: Vec<u8>) {
     let cur = Cursor::new(&msgpack_buf[..]);
     let mut deserializer = Deserializer::new(cur);
@@ -79,7 +124,7 @@ pub fn decode_msgpack_body(msgpack_buf: Vec<u8>) {
         Result::Ok(res) => {
             let message: Message = res;
             println!("message = {:?}", message);
-            //test_response();
+            test_response();
         }
         Result::Err(err) =>
             println!("error parsing: {:?}", err)
