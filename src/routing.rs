99b0d04d6ef16c28fef7df165bd83bcc760fc7e5
James N. V. Cash <james.nvc@gmail.com>
2016-06-11 12:39:19 -0400

    add server errors for mac failure

--- src/routing.rs
+++ src/routing.rs
@@ -14,3 +14,29 @@ impl fmt::Display for NoRoute {
 impl Error for NoRoute {
     fn description(&self) -> &str { "No Route" }
 }
+
+#[derive(Debug)]
+pub struct MissingMac;
+
+impl fmt::Display for MissingMac {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Missing X-Braid-Signature header")
+    }
+}
+
+impl Error for MissingMac {
+    fn description(&self) -> &str { "Missing signature header" }
+}
+
+#[derive(Debug)]
+pub struct BadMac;
+
+impl fmt::Display for BadMac {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Mac check failed")
+    }
+}
+
+impl Error for BadMac {
+    fn description(&self) -> &str { "Bad signature header" }
+}
